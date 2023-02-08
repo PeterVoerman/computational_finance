4da9781ca6444e54249259a100b7b8a295f3b4af
PeterVoerman <peter.jpv@hotmail.com>
2023-02-08 11:19:37 +0100

    1.2

---
+++ ass_1.py
@@ -0,0 +1,58 @@
+import numpy as np
+
+def buildTree(S, vol, T, N):
+    dt = T / N
+
+    matrix = np.zeros((N + 1, N + 1))
+
+    u = np.exp(vol * np.sqrt(dt))
+    d = 1 / u
+
+    for i in range(N + 1):
+        for j in range(i + 1):
+            matrix[i, j] = S * (u ** (j)) * (d ** (i-j))
+
+    return matrix
+
+sigma = 0.1
+S = 80
+T = 1
+N = 2
+
+print(buildTree(S, sigma, T, N))
+
+def valueOptionMatrix(tree, T, r, K, vol):
+    dt = T / N
+
+    u = np.exp(vol * np.sqrt(dt))
+    d = 1 / u
+
+    p = (np.exp(r * dt) - d) / (u - d)
+
+    columns = tree.shape[1]
+    rows = tree.shape[0]
+
+    for c in np.arange(columns):
+        S = tree[rows - 1, c]
+        tree[rows - 1, c] = max(S - K, 0)
+
+    
+    for i in np.arange(rows - 1)[::-1]:
+        for j in np.arange(i + 1):
+            down = tree[i + 1, j]
+            up = tree[i + 1, j + 1]
+            tree[i, j] = np.exp(-r * dt) * (p * up + (1 - p) * down)
+
+    return tree
+
+sigma = 0.1
+S = 80
+T = 1
+N = 2
+
+K = 85
+r = 0.1
+
+tree = buildTree(S, sigma, T, N)
+print(valueOptionMatrix(tree, T, r, K, sigma))
+
