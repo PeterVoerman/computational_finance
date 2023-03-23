811ed627e407022e4c071b4176193e1b3774507b
tdiaz5 <tobiasdiazdenhollander@live.nl>
2023-03-23 17:22:29 +0100

    working ftcs

--- assignment_3/finite_diff.py
+++ assignment_3/finite_diff.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy import sparse
 import matplotlib.pyplot as plt
+from scipy.stats import norm
 
 def finite_diff_ftcs(S0, K, r, sigma, T, dx, dt, M1, M2):
     Nt = int(T / dt)
@@ -10,21 +11,43 @@ def finite_diff_ftcs(S0, K, r, sigma, T, dx, dt, M1, M2):
     A_diag_2 = (1 - sigma ** 2 * dt / dx ** 2 - r * dt) * np.ones(Nx)
     A_diag_3 = ((r - 0.5 * sigma ** 2) * dt / (2 * dx) + 0.5 * sigma ** 2 * dt / dx ** 2) * np.ones(Nx - 1)
 
+    A_diag_2[0] = 0
+    A_diag_3[0] = 0
+
+    A_diag_1[-1] = 0
+    A_diag_2[-1] = 0
+
+    offset = np.zeros(Nx)
+    offset[-1] = np.exp(M2)
+
     A = sparse.diags([A_diag_1, A_diag_2, A_diag_3], [-1, 0, 1], format='csc')
 
-    print(A.toarray())
+    # print(A.toarray())
 
-    initial_V = np.full(Nx, max(S0 - K, 0))
+    V = np.full(Nx, max(S0 - K, 0))
 
+    for n in range(Nt):
+        V = A @ V + offset
 
+    # print(V.shape)
+    # print(np.linspace(-M1, M2, d))
+    S = np.exp(np.linspace(-M1, M2, Nx))
+    d1 = (np.log(S / K) + (r + sigma ** 2 / 2) * T) / (sigma * np.sqrt(T))
+    d2 = d1 - sigma * np.sqrt(T)
+    priceAnalytical = S * norm.cdf(d1) - K * np.exp(-r * T) * norm.cdf(d2)
+
+    plt.plot(S, V, label="numeric")
+    plt.plot(S, priceAnalytical, label="bs")
+    plt.legend()
+    plt.show()
 
 S0 = 100
 K = 100
 r = 0.05
 sigma = 0.2
 T = 1
-dx = 0.1
-dt = 0.01
+dx = 0.001
+dt = 0.0001
 M1 = 10
 M2 = 10
 
